3178a47c409eb5009c79d8be66af39bf0c20569d
Your Name <you@example.com>
2018-04-23 19:46:24 +0600

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace blog.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+                // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+                // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
