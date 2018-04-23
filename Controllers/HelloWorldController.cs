fbb835a25ebab37095349015f2ff0c997549841f
Your Name <you@example.com>
2018-04-23 20:58:27 +0600

    Create Controllers

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,25 +5,25 @@ namespace blog.Controllers
 {
     public class HelloWorldController : Controller
     {
-                // 
+                
         // GET: /HelloWorld/
 
         public string Index()
         {
             return "This is my default action...";
         }
-        // public IActionResult Index()
-        // {
-        //     return View();
-        // }
-                // 
+//         // public IActionResult Index()
+//         // {
+//         //     return View();
+//         // }
+//                 // 
         // GET: /HelloWorld/Welcome/ 
 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
+        // public string Welcome()
+        // {
+        //     return "This is the Welcome action method...";
+        // }
         public string Welcome(string name, int numTimes = 1)
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
